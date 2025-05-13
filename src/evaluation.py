80e058945e2c02b3691df2f0ff5cc6d4609f355e
Lujain Mansour <89729949+lujy12@users.noreply.github.com>
2025-05-13 19:41:20 +0300

    Delete src/evaluation.py

--- src/evaluation.py
+++
@@ -1,8 +0,0 @@
-def evaluate(query, expected, actual):
-    expected = expected.lower()
-    actual = actual.lower()
-    tp = int(expected in actual)
-    precision = tp
-    recall = tp
-    f1 = 2 * precision * recall / (precision + recall + 1e-6)
-    return precision, recall, f1
